3420e4aa306daac1c854fef95109b75c693fd265
Ricardo Assing <ricardo@tsiana.ca>
2025-03-20 03:16:48 -0400

    Added class constants for Core fields

--- src/Api/Contacts/Fields/Core.php
+++ src/Api/Contacts/Fields/Core.php
@@ -6,6 +6,26 @@ class Core extends \ArrayObject
 {
     protected array $data;
 
+    public const FIELD_TITLE = "title";
+    public const FIELD_FIRSTNAME = "firstname";
+    public const FIELD_LASTNAME = "lastname";
+    public const FIELD_EMAIL = "email";
+    public const FIELD_COMPANY = "company";
+    public const FIELD_POSITION = "position";
+    public const FIELD_PHONE = "phone";
+    public const FIELD_MOBILE = "mobile";
+    public const FIELD_WEBSITE = "website";
+    public const FIELD_POINTS = "points";
+    public const FIELD_ADDRESS1 = "address1";
+    public const FIELD_ADDRESS2 = "address2";
+    public const FIELD_CITY = "city";
+    public const FIELD_STATE = "state";
+    public const FIELD_ZIPCODE = "zipcode";
+    public const FIELD_COUNTRY = "country";
+    public const FIELD_TIMEZONE = "timezone";
+    public const FIELD_ATTRIBUTION_DATE = "attribution_date";
+    public const FIELD_ATTRIBUTION = "attribution";
+
     public function __construct(array $data)
     {
         $this->data = $data;
@@ -19,210 +39,210 @@ class Core extends \ArrayObject
 
     public function getTitle(bool $returnObject = false): string|Field|null
     {
-        if (!isset($this->data['title'])) return '';
+        if (!isset($this->data[self::FIELD_TITLE])) return '';
 
         if ($returnObject) {
-            return new Field($this->data['title']);
+            return new Field($this->data[self::FIELD_TITLE]);
         } else {
-            return $this->data['title']['value'];
+            return $this->data[self::FIELD_TITLE]['value'];
         }
     }
 
     public function getFirstName(bool $returnObject = false): string|Field|null
     {
-        if (!isset($this->data['firstname'])) return '';
+        if (!isset($this->data[self::FIELD_FIRSTNAME])) return '';
 
         if ($returnObject) {
-            return new Field($this->data['firstname']);
+            return new Field($this->data[self::FIELD_FIRSTNAME]);
         } else {
-            return $this->data['firstname']['value'];
+            return $this->data[self::FIELD_FIRSTNAME]['value'];
         }
     }
 
     public function getLastName(bool $returnObject = false): string|Field|null
     {
-        if (!isset($this->data['lastname'])) return '';
+        if (!isset($this->data[self::FIELD_LASTNAME])) return '';
 
         if ($returnObject) {
-            return new Field($this->data['lastname']);
+            return new Field($this->data[self::FIELD_LASTNAME]);
         } else {
-            return $this->data['lastname']['value'];
+            return $this->data[self::FIELD_LASTNAME]['value'];
         }
     }
 
     public function getEmail(bool $returnObject = false): string|Field
     {
-        if (!isset($this->data['email'])) return '';
+        if (!isset($this->data[self::FIELD_EMAIL])) return '';
 
         if ($returnObject) {
-            return new Field($this->data['email']);
+            return new Field($this->data[self::FIELD_EMAIL]);
         } else {
-            return $this->data['email']['value'];
+            return $this->data[self::FIELD_EMAIL]['value'];
         }
     }
 
     public function getCompany(bool $returnObject = false): string|Field|null
     {
-        if (!isset($this->data['company'])) return '';
+        if (!isset($this->data[self::FIELD_COMPANY])) return '';
 
         if ($returnObject) {
-            return new Field($this->data['company']);
+            return new Field($this->data[self::FIELD_COMPANY]);
         } else {
-            return $this->data['company']['value'];
+            return $this->data[self::FIELD_COMPANY]['value'];
         }
     }
 
     public function getPosition(bool $returnObject = false): string|Field|null
     {
-        if (!isset($this->data['position'])) return '';
+        if (!isset($this->data[self::FIELD_POSITION])) return '';
 
         if ($returnObject) {
-            return new Field($this->data['position']);
+            return new Field($this->data[self::FIELD_POSITION]);
         } else {
-            return $this->data['position']['value'];
+            return $this->data[self::FIELD_POSITION]['value'];
         }
     }
 
     public function getPhone(bool $returnObject = false): string|Field|null
     {
-        if (!isset($this->data['phone'])) return '';
+        if (!isset($this->data[self::FIELD_PHONE])) return '';
 
         if ($returnObject) {
-            return new Field($this->data['phone']);
+            return new Field($this->data[self::FIELD_PHONE]);
         } else {
-            return $this->data['phone']['value'];
+            return $this->data[self::FIELD_PHONE]['value'];
         }
     }
 
     public function getMobile(bool $returnObject = false): string|Field|null
     {
-        if (!isset($this->data['mobile'])) return '';
+        if (!isset($this->data[self::FIELD_MOBILE])) return '';
 
         if ($returnObject) {
-            return new Field($this->data['mobile']);
+            return new Field($this->data[self::FIELD_MOBILE]);
         } else {
-            return $this->data['mobile']['value'];
+            return $this->data[self::FIELD_MOBILE]['value'];
         }
     }
 
     public function getWebsite(bool $returnObject = false): string|Field|null
     {
-        if (!isset($this->data['website'])) return '';
+        if (!isset($this->data[self::FIELD_WEBSITE])) return '';
 
         if ($returnObject) {
-            return new Field($this->data['website']);
+            return new Field($this->data[self::FIELD_WEBSITE]);
         } else {
-            return $this->data['website']['value'];
+            return $this->data[self::FIELD_WEBSITE]['value'];
         }
     }
 
     public function getPoints(bool $returnObject = false): int|Field|null
     {
-        if (!isset($this->data['points'])) return 0;
+        if (!isset($this->data[self::FIELD_POINTS])) return 0;
 
         if ($returnObject) {
-            return new Field($this->data['points']);
+            return new Field($this->data[self::FIELD_POINTS]);
         } else {
-            return $this->data['points']['value'];
+            return $this->data[self::FIELD_POINTS]['value'];
         }
     }
 
     public function getAddress1(bool $returnObject = false): string|Field|null
     {
-        if (!isset($this->data['address1'])) return '';
+        if (!isset($this->data[self::FIELD_ADDRESS1])) return '';
 
         if ($returnObject) {
-            return new Field($this->data['address1']);
+            return new Field($this->data[self::FIELD_ADDRESS1]);
         } else {
-            return $this->data['address1']['value'];
+            return $this->data[self::FIELD_ADDRESS1]['value'];
         }
     }
 
     public function getAddress2(bool $returnObject = false): string|Field|null
     {
-        if (!isset($this->data['address2'])) return '';
+        if (!isset($this->data[self::FIELD_ADDRESS2])) return '';
 
         if ($returnObject) {
-            return new Field($this->data['address2']);
+            return new Field($this->data[self::FIELD_ADDRESS2]);
         } else {
-            return $this->data['address2']['value'];
+            return $this->data[self::FIELD_ADDRESS2]['value'];
         }
     }
 
     public function getCity(bool $returnObject = false): string|Field|null
     {
-        if (!isset($this->data['city'])) return '';
+        if (!isset($this->data[self::FIELD_CITY])) return '';
 
         if ($returnObject) {
-            return new Field($this->data['city']);
+            return new Field($this->data[self::FIELD_CITY]);
         } else {
-            return $this->data['city']['value'];
+            return $this->data[self::FIELD_CITY]['value'];
         }
     }
 
     public function getState(bool $returnObject = false): string|Field|null
     {
-        if (!isset($this->data['state'])) return '';
+        if (!isset($this->data[self::FIELD_STATE])) return '';
 
         if ($returnObject) {
-            return new Field($this->data['state']);
+            return new Field($this->data[self::FIELD_STATE]);
         } else {
-            return $this->data['state']['value'];
+            return $this->data[self::FIELD_STATE]['value'];
         }
     }
 
     public function getZipcode(bool $returnObject = false): string|Field|null
     {
-        if (!isset($this->data['zipcode'])) return '';
+        if (!isset($this->data[self::FIELD_ZIPCODE])) return '';
 
         if ($returnObject) {
-            return new Field($this->data['zipcode']);
+            return new Field($this->data[self::FIELD_ZIPCODE]);
         } else {
-            return $this->data['zipcode']['value'];
+            return $this->data[self::FIELD_ZIPCODE]['value'];
         }
     }
 
     public function getCountry(bool $returnObject = false): string|Field|null
     {
-        if (!isset($this->data['country'])) return '';
+        if (!isset($this->data[self::FIELD_COUNTRY])) return '';
 
         if ($returnObject) {
-            return new Field($this->data['country']);
+            return new Field($this->data[self::FIELD_COUNTRY]);
         } else {
-            return $this->data['country']['value'];
+            return $this->data[self::FIELD_COUNTRY]['value'];
         }
     }
 
     public function getTimezone(bool $returnObject = false): string|Field|null
     {
-        if (!isset($this->data['timezone'])) return '';
+        if (!isset($this->data[self::FIELD_TIMEZONE])) return '';
 
         if ($returnObject) {
-            return new Field($this->data['timezone']);
+            return new Field($this->data[self::FIELD_TIMEZONE]);
         } else {
-            return $this->data['timezone']['value'];
+            return $this->data[self::FIELD_TIMEZONE]['value'];
         }
     }
 
     public function getAttributionDate(bool $returnObject = false): string|Field|null
     {
-        if (!isset($this->data['attribution_date'])) return '';
+        if (!isset($this->data[self::FIELD_ATTRIBUTION_DATE])) return '';
 
         if ($returnObject) {
-            return new Field($this->data['attribution_date']);
+            return new Field($this->data[self::FIELD_ATTRIBUTION_DATE]);
         } else {
-            return $this->data['attribution_date']['value'];
+            return $this->data[self::FIELD_ATTRIBUTION_DATE]['value'];
         }
     }
 
     public function getAttribution(bool $returnObject = false): string|Field|null
     {
-        if (!isset($this->data['attribution'])) return '';
+        if (!isset($this->data[self::FIELD_ATTRIBUTION])) return '';
 
         if ($returnObject) {
-            return new Field($this->data['attribution']);
+            return new Field($this->data[self::FIELD_ATTRIBUTION]);
         } else {
-            return $this->data['attribution']['value'];
+            return $this->data[self::FIELD_ATTRIBUTION]['value'];
         }
     }
 }
